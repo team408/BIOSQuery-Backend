1359dd53be523091a3f2e2d6e4a210f2b0284292
TheMaziland <alon951m@gmail.com>
2024-04-22 13:12:48 +0300

    Created a fleet service with an ApiRequest for fleet server

---
+++ services/fleet.js
@@ -0,0 +1,23 @@
+const axios = require('axios').default;
+
+async function getRequest(url, headers = null) {
+    try {
+        if (headers) {
+            response = (await axios.get(url, { headers: headers }));
+            return response.data;
+        } else {
+            response = (await axios.get(url));
+            return response.data;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function fleetApiRequest(uri) {
+    fleetUrl = `https://${process.env.FLEET_SERVER_IP}:${process.env.FLEET_SERVER_PORT}`;
+    response = await getRequest(fleetUrl + endpointsUri, headers = { "Authorization": `Bearer ${process.env.FLEET_API_TOKEN}` })
+    return response
+};
+
+module.exports = { fleetApiRequest, getRequest };
