0d96e308338d28bdf35f6fb49a97474a4e2130d7
TheMaziland <alon951m@gmail.com>
2024-04-23 10:53:21 +0300

    Added exception handling to getEndpoints

--- controllers/endpoints.js
+++ controllers/endpoints.js
@@ -1,9 +1,16 @@
 const fleetService = require('../services/fleet');
 
 async function getEndpoints(req, res) {
-    endpointsUri = '/api/v1/fleet/hosts?page=0&per_page=100'
-    endpoints = await fleetService.fleetApiRequest(endpointsUri);
-    res.render("endpoints.ejs", { endpoints: endpoints.hosts });
+    try {
+
+        endpointsUri = '/api/v1/fleet/hosts?page=0&per_page=100'
+        endpoints = await fleetService.fleetApiRequest(endpointsUri);
+        res.render("endpoints.ejs", { endpoints: endpoints.hosts });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 module.exports = { getEndpoints };
