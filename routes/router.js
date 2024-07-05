d09e79a88eded7940fc679b6825fb7d2b26d0f72
[tal azary] <[azary.tal@gmail.com]>
2024-07-05 18:37:35 +0300

    agent controller and agent installation base
    the commands are all ready to use to enroll a new agent.
    need to figure out the correct code architecture (view,controller,service) to run this on a remote host (via ssh)

--- routes/router.js
+++ routes/router.js
@@ -4,11 +4,15 @@ const router = express.Router();
 // Controllers
 const homepageController = require("../controllers/homepage");
 const endpointsController = require("../controllers/endpoints");
+const agentsController = require("../controllers/agents");
 
 // Main route
 router.get("/", homepageController.homepage);
 
 // Endpoints
 router.get("/endpoints", endpointsController.getEndpoints);
+
+// Agents
+router.get("/api/agents/addNode", agentsController.addNode);
 
 module.exports = router;
