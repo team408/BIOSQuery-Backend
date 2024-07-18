6188d335d59c84169759c5a34dcf6e5032979243
Gayaossi <ossigaya@gmail.com>
2024-07-18 10:47:58 +0300

    message

--- controllers/endpoints.js
+++ controllers/endpoints.js
@@ -46,5 +46,44 @@ async function getEndpoints(req, res) {
         res.status(500).send('Internal Server Error');
     }
 }
+async function addNode(req, res) {
+    try {
+        //validate hostId
+        const hostId = req.params.hostId;
+        const osType = req.params.osType;
+        if (!hostId || !osType) {
+            return res.status(400).send({ error: 'hostID parameter is required' });
+        }
 
-module.exports = { getEndpoints };
+        // Validate correct osType request
+        if (!(['deb', 'rpm', 'pkg', 'msi'].includes(osType))) {
+            res.status(404).send('Unknown osType');
+            return;
+        }
+        if ((['pkg', 'msi'].includes(osType))) {
+            res.status(404).send('unsupported osType, yet.');
+            return;
+        }
+
+        // Get EnrollmentCmd
+        const enrollCmd = await fleetService.getAgentEnrollCmd(osType);
+
+        // Execute enrollment command
+        await systemService.remoteEnrollLinuxHost(enrollCmd, hostId);
+        res.send("Node enrolled successfully");
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function getControlPanel(req, res) {
+    try {
+        res.render("control.ejs");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+module.exports = { getEndpoints, addNode, getControlPanel };
--- routes/router.js
+++ routes/router.js
@@ -18,17 +18,15 @@ router.get("/endpoints", endpointsController.getEndpoints);
 router.get("/statistics", dashboardController.statistics);
 
 // Agents
-router.post("/api/agents/addNode/:osType/:hostId", agentsController.addNode);
+router.post("/api/agents/addNode/:osType/:hostId", (req, res, next) => {
+    console.log('addNode route called');
+    next();
+}, agentsController.addNode);
 
-// Chipsec
-router.get("/api/chipsec/install/:hostId", chipsecController.installChipsec);
-router.get("/api/chipsec/uninstall/:hostId", chipsecController.uninstallChipsec);
-router.get("/api/chipsec/runModule/:module/:hostId", chipsecController.runModule);
-
-
-// Agents
-router.post("/api/agents/addNode/:osType/:hostId", agentsController.addNode);
-router.get("/controlPanel", agentsController.getControlPanel)
+router.get("/controlPanel", (req, res, next) => {
+    console.log('getControlPanel route called');
+    next();
+}, agentsController.getControlPanel);
 
 // Chipsec
 router.get("/api/chipsec/install/:hostId", chipsecController.installChipsec);
