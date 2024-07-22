beac63c571412b8cc2bc3e56e9c5e81a40b786a6
Alon Mazor <alon@oceansecurity.io>
2024-07-22 09:12:19 +0300

    Implemented the install chipsec API call, with toasts messages

--- controllers/chipsec.js
+++ controllers/chipsec.js
@@ -1,14 +1,34 @@
 const fleetService = require('../services/fleet');
+const axios = require('axios').default;
 
 async function installChipsec(req, res) {
     try {
-        //run fleet api to run fleet script to install chipsec
-        // response = await fleetService.fleetApiGetRequest(/**enter here api to run scripts */)
-        res.status(200).send("Chipsec Installed Successfuly!")
+        scripts = await fleetService.listScripts();
+        installChipsecScriptId = scripts.find(script => script.name === "install_chipsec.sh").id
+        installDependenciesScriptId = scripts.find(script => script.name === "install_dependencies.sh").id
+
+        hostId = req.params.hostId
+        let data=`{"host_id": ${hostId}, "script_id": ${installDependenciesScriptId}}`;
+        response = await fleetService.fleetApiPostRequest("/api/latest/fleet/scripts/run", data=data)
+        if (response.data.execution_id){
+            res.status(200).send({"result": "Chipsec install command sent!"})
+        } else {
+            res.status(500).send({"result": "Failed to send install command"})
+        }
 
     } catch (error) {
-        console.log(error)
-        res.status(500).send('Internal Server Error');
+        if (error.response){
+            console.log(error.response.data);
+            res.status(error.response.status).send({"result":error.response.data});
+            return
+        }
+        console.log(error);
+        res.status(500).send(
+            {
+                'result':'Internal Server Error'
+            }
+        );
+        return;
     }
 };
 
--- public/js/endpoints.js
+++ public/js/endpoints.js
@@ -7,43 +7,56 @@ $(document).ready(function() {
         let api_url = '';
         // create custom message based on a switch statement
         let toast_message = '';
-        let endpoint_id = $(this).attr('id');
-        switch (endpoint_id) {
+        let action_name = $(this).attr('id');
+        let endpoint_host_id = $(this).closest('.card-body').find('#endpoint_id').text().trim()
+        switch (action_name) {
             case 'install_chipsec':
-                toast_message = `Installing Chipsec on host ${this.closest('.card-body').querySelector('.card-title').textContent}`;
-                api_url = `/api/chipsec/install/${endpoint_id}`;
+                api_url = `/api/chipsec/install/${endpoint_host_id}`;
                 break;
             case 'uninstall_chipsec':
-                toast_message = `Uninstalling Chipsec on host ${this.closest('.card-body').querySelector('.card-title').textContent}`;
-                api_url = `/api/chipsec/uninstall/${endpoint_id}`;
+                // TODO: implement
+                api_url = `/api/chipsec/uninstall/${endpoint_host_id}`;
                 break;
             case 'execute_script':
-                toast_message = `Executing script on host ${this.closest('.card-body').querySelector('.card-title').textContent}`;
-                api_url = `/api/chipsec/run/${endpoint_id}`;
+                // TODO: implement
+                api_url = `/api/chipsec/run/${endpoint_host_id}`;
                 break;
             default:
                 toast_message = '';
         }
 
         $.ajax({
-            url: `/api/chipsec/install/${$(this).closest('.card-body').find('#endpoint_id').text().trim()}`,
+            // url: `/api/chipsec/install/${$(this).closest('.card-body').find('#endpoint_id').text().trim()}`,
+            url: api_url,
             method: 'GET',
             success: function (response) {
                 // Handle success response
-                const container = document.getElementById('cart-toast-container');
+                const container = document.getElementById('toast-container');
                 const targetElement = document.querySelector('[data-kt-docs-toast="stack"]');
                 const newToast = targetElement.cloneNode(true);
-        
+                
                 // Update title and message
+                newToast.classList.add('toast-success');
                 newToast.querySelector('.toast-header strong').textContent = customTitle;
-                newToast.querySelector('.toast-body').textContent = toast_message;
+                newToast.querySelector('.toast-body').textContent = response.result;
         
                 container.append(newToast);
                 const toast = bootstrap.Toast.getOrCreateInstance(newToast);
                 toast.show();
             },
             error: function (error) {
-                // Handle error response
+                const container = document.getElementById('toast-container');
+                const targetElement = document.querySelector('[data-kt-docs-toast="stack"]');
+                const newToast = targetElement.cloneNode(true);
+
+                newToast.classList.add('toast-error');
+                newToast.querySelector('.toast-header strong').textContent = 'Action Failed';
+                newToast.querySelector('.toast-body').textContent = 'An error occurred. Please try again.';
+
+                container.append(newToast);
+                const toast = bootstrap.Toast.getOrCreateInstance(newToast);
+                toast.show();
+
                 console.log(error);
             }
         });
