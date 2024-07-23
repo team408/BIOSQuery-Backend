a4243222a142a32334179f001c3d7c3f60b17b4d
Alon Mazor <alon@oceansecurity.io>
2024-07-23 08:22:56 +0300

    Added chipsec uninstall

--- controllers/chipsec.js
+++ controllers/chipsec.js
@@ -13,9 +13,9 @@ async function installChipsec(req, res) {
         response = await fleetService.fleetApiPostRequest("/api/latest/fleet/scripts/run", data=install_deps_data)
         response = await fleetService.fleetApiPostRequest("/api/latest/fleet/scripts/run", data=install_chipsec_data)
         if (response.data.execution_id){
-            res.status(200).send({"result": "Chipsec install command sent!"})
+            res.status(200).send({"result": "Chipsec install command sent successfuly!"})
         } else {
-            res.status(500).send({"result": "Failed to send install command"})
+            res.status(500).send({"result": "Failed to send command"})
         }
 
     } catch (error) {
@@ -36,13 +36,31 @@ async function installChipsec(req, res) {
 
 async function uninstallChipsec(req, res) {
     try {
-        //run fleet api to run fleet script to uninstall chipsec
-        // response = await fleetService.fleetApiGetRequest(/**enter here api to run scripts */)
-        res.status(200).send("Chipsec Uninstalled Successfuly!")
+        scripts = await fleetService.listScripts();
+        uninstallChipsecScriptId = scripts.find(script => script.name === "uninstall_chipsec.sh").id
+
+        hostId = req.params.hostId
+        let uninstall_chipsec_data=`{"host_id": ${hostId}, "script_id": ${uninstallChipsecScriptId}}`;
+        response = await fleetService.fleetApiPostRequest("/api/latest/fleet/scripts/run", data=uninstall_chipsec_data)
+        if (response.data.execution_id){
+            res.status(200).send({"result": "Chipsec uninstall command sent successfuly!"})
+        } else {
+            res.status(500).send({"result": "Failed to send command"})
+        }
 
     } catch (error) {
-        console.log(error)
-        res.status(500).send('Internal Server Error');
+        if (error.response){
+            console.log(error.response.data);
+            res.status(error.response.status).send({"result":error.response.data});
+            return
+        }
+        console.log(error);
+        res.status(500).send(
+            {
+                'result':'Internal Server Error'
+            }
+        );
+        return;
     }
 };
 
