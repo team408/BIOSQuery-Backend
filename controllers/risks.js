3e2a1d7aca63a8a924c82a9109b3beb03768db91
Alon Mazor <alon@oceansecurity.io>
2024-08-25 12:09:38 +0300

    changed error string on status 500

--- controllers/risks.js
+++ controllers/risks.js
@@ -45,7 +45,7 @@ async function getEndpointRiskInfo(req, res) {
         }
     } catch (error) {
             console.log(error);
-            res.status(500).send({"error": "Failed to get risk info"});
+            res.status(500).send({"error": "Internal Server Error"});
     }
 }
 
