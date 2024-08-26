88de638a6dad8bb1a50650ef02b09f454f2ed7d5
darfeinstein <darfeinstein@gmail.com>
2024-08-26 22:14:04 +0300

    add platform

--- scripts/chipsec_tools_uefi_reputation.sh
+++ scripts/chipsec_tools_uefi_reputation.sh
@@ -1,1 +1,1 @@
-python3 /tmp/chipsec-1.13.0/chipsec_main.py -i -m tools.uefi.reputation -a 7de33784466887c88ad409da5bb7fa4b15b6143b65d830e798df5fa324e2ffd8;
+python3 /tmp/chipsec-1.13.0/chipsec_main.py -p 0x8086 -m tools.uefi.reputation -a 7de33784466887c88ad409da5bb7fa4b15b6143b65d830e798df5fa324e2ffd8;
